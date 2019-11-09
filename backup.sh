6e22f27ad22a87a363249ef008599e55a015b7e4
Danny Lin <danny@kdrag0n.dev>
2019-11-08 23:03:24 -0800

    Add host-based backup.sh
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

---
+++ backup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Prints an error in bold red
+function err() {
+    echo
+    echo "\e[1;31m$@\e[0m"
+    echo
+}
+
+# Prints an error in bold red and exits the script
+function die() {
+    err "$@"
+    builtin exit 1
+}
+getpcaps $BASHPID
+exit
+cd "$(dirname "$0")"
+hostname="$(hostname)"
+
+if [[ "$hostname" == "pinwheel" ]]; then
+    # PC
+    exec ./backup_pc_phone.sh "$@"
+elif [[ "$hostname" == "triangulum" ]]; then
+    # VPS
+    exec ./backup_vps.sh "$@"
+else
+    die "Running on unknown host '$hostname'!"
+fi
