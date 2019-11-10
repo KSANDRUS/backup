29a2977c6c206b2d715df2832ea96befc44a7941
Danny Lin <danny@kdrag0n.dev>
2019-11-10 19:04:58 +0000

    User and shcap ctx
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- shcap.sh
+++ shcap.sh
@@ -5,9 +5,11 @@ set -e
 login_shell="$(getent passwd $LOGNAME | cut -d: -f7)"
 
 exec sudo systemd-run \
-    -p User="$(id -u)" -p Group="$(id -g)" \
+    -p User="$(id -u)" \
+    -p Group="$(id -g)" \
     -p CapabilityBoundingSet=CAP_DAC_READ_SEARCH \
     -p AmbientCapabilities=CAP_DAC_READ_SEARCH \
     -p WorkingDirectory="$PWD" \
+    -p Environment=VIRTUAL_ENV=shcap \
     -t \
     "$login_shell"
